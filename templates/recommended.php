476d5f60c097cdab3939544c46f2a6d13729adb5
Eric Sherred <esherred@gmail.com>
2017-01-02 11:30:45 -0500

    syntax

--- templates/recommended.php
+++ templates/recommended.php
@@ -1,31 +1,27 @@
-<div class="mdl-grid">
-    <div class="mdl-layout-spacer"></div>
-    <section class="mdl-cell mdl-cell--8-col recommended">
-      <div class="mdl-grid">
-        <h2 class="mdl-cell mdl-cell--12-col center">Recommended Stories</h2>
-        
-        <?php
-          $categories = get_the_category( $post->ID );
-          if ( $categories ) {
-            $first_category = $categories[0]->term_id;
-            $args = array(
-              'category__in'     => array( $first_category ),
-              'post__not_in'     => array( $post->ID ),
-              'posts_per_page'   => 2,
-              'caller_get_posts' => 1,
-            );
-            $my_query = new WP_Query( $args );
-            if( $my_query->have_posts() ) {
-              while ($my_query->have_posts()) : $my_query->the_post() {
-                get_template_part( 'templates/recommended', 'post' );
-              }
-            }
-            wp_reset_query();
-            }
-          ?>
+<section class="mdl-cell mdl-cell--8-col recommended">
+  <div class="mdl-grid">
+    <h2 class="mdl-cell mdl-cell--12-col center">Recommended Stories</h2>
+    
+    <?php
+      $categories = get_the_category( $post->ID );
+      if ( $categories ) {
+        $first_category = $categories[0]->term_id;
+        $args = array(
+          'category__in'     => array( $first_category ),
+          'post__not_in'     => array( $post->ID ),
+          'posts_per_page'   => 2,
+          'caller_get_posts' => 1,
+        );
+        $my_query = new WP_Query( $args );
+        if( $my_query->have_posts() ) {
+          while ($my_query->have_posts()) {
+            $my_query->the_post();
+            get_template_part( 'templates/teaser' );
+          }
+        }
+        wp_reset_query();
+      }
+      ?>
 
-      </div>
-    </section>
-    <div class="mdl-layout-spacer"></div>
   </div>
-</div>
+</section>
