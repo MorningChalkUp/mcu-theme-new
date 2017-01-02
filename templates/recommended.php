f4332d7b02b6b1c3e58fc9be745ff56f0c75fdcb
Eric Sherred <esherred@gmail.com>
2017-01-02 03:15:52 -0500

    spelling is hard, or is it typing?

--- templates/recommended.php
+++ templates/recommended.php
@@ -17,7 +17,7 @@
             $my_query = new WP_Query( $args );
             if( $my_query->have_posts() ) {
               while ($my_query->have_posts()) : $my_query->the_post() {
-                get_template_part( 'templates/recommented', 'post' );
+                get_template_part( 'templates/recommended', 'post' );
               }
             }
             wp_reset_query();
