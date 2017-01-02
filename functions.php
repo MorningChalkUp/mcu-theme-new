6758c221648ac49e69e27f987594277d076b42b4
Eric Sherred <esherred@gmail.com>
2017-01-02 03:05:31 -0500

    oops

--- functions.php
+++ functions.php
@@ -3,29 +3,29 @@
 add_theme_support('post-thumbnails'); 
 
 function mcu_get_nav() {
-  include get_template_directory_uri() . 'templates/nav.php';
+  include get_template_directory_uri() . '/templates/nav.php';
 }
 
 function mcu_get_fb() {
-  include get_template_directory_uri() . 'templates/fb.php';
+  include get_template_directory_uri() . '/templates/fb.php';
 }
 
 function mcu_get_post() {
-  include get_template_directory_uri() . 'templates/post.php';
+  include get_template_directory_uri() . '/templates/post.php';
 }
 
 function mcu_get_recommended() {
-  include get_template_directory_uri() . 'templates/recommended.php';
+  include get_template_directory_uri() . '/templates/recommended.php';
 }
 
 function mcu_get_recommended_post() {
-  include get_template_directory_uri() . 'templates/recommended_post.php';
+  include get_template_directory_uri() . '/templates/recommended_post.php';
 }
 
 function mcu_get_subscribe() {
-  include get_template_directory_uri() . 'templates/subscribe.php';
+  include get_template_directory_uri() . '/templates/subscribe.php';
 }
 
 function mcu_get_footer() {
-  include get_template_directory_uri() . 'templates/footer.php';
+  include get_template_directory_uri() . '/templates/footer.php';
 }
