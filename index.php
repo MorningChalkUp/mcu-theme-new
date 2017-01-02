d4872bea889955a8222abf55c5d502ca28fb20cd
Eric Sherred <esherred@gmail.com>
2017-01-02 12:16:34 -0500

    update index temp

--- index.php
+++ index.php
@@ -1,34 +1,53 @@
 <?php get_header(); ?>
 
-<main class="mdl-layout__content">
-  <div class="page-content">
-    <div class="mdl-grid">
-      <div class="mdl-layout-spacer"></div>
-
-      <?php
-        if ( have_posts() ) {
-          while ( have_posts() ) {
-            the_post();
-            get_template_part( 'templates/post' );
-          }
-        }
-      ?>
-
-      <div class="mdl-layout-spacer"></div>
-    </div>
-    <div class="mdl-grid">
-      <div class="mdl-layout-spacer"></div>
+  <main class="mdl-layout__content">
+    <div class="page-content">
+      <div class="mdl-grid">
+        <section class="mdl-cell mdl-cell--12-col main archive">
+          <div class="mdl-grid">
+            <div class="mdl-layout-spacer"></div>
+            <div class="mdl-cell mdl-cell--8-col title">
+              <h1><?php the_archive_title(); ?></h1>
+              <p><?php the_archive_description(); ?></p>
+            </div>
+            <div class="mdl-layout-spacer"></div>
+          </div>
+          <div class="mdl-grid">
+            <?php query_posts('showposts=9');
+            if (have_posts()) {
+              while (have_posts()) {
+                the_post();
+                get_template_part( 'templates/teaser', 'archive' );
+              }
+            } 
+            ?>
+          </div>
+        </section>
+      </div>
       
-      <?php get_template_part( 'templates/recommended' ); ?>
+      <?php get_template_part( 'templates/subscribe' ); ?>
 
-      <div class="mdl-layout-spacer"></div>
+      <div class="mdl-grid">
+        <section class="mdl-cell mdl-cell--12-col main archive">
+          <div class="mdl-grid">
+            <?php query_posts( array(
+                'offset' => 9,
+                'posts_per_page' => -1,
+              ) );
+              if (have_posts()) {
+                while (have_posts()) {
+                  the_post();
+                  get_template_part( 'templates/teaser', 'archive' );
+                }
+              } 
+            ?>
+          </div>
+        </section>
+      </div>
     </div>
-  </div>
 
-  <?php get_template_part( 'templates/subscribe' ); ?>
-
-  <?php get_template_part( 'templates/footer' ); ?>
-
-</main>
+    <?php get_template_part( 'templates/footer' ); ?>
+    
+  </main>
 
 <?php get_footer(); ?>
