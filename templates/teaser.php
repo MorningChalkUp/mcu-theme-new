deaac0e051e017e884c61da094e7dd0b36ea5164
Eric Sherred <esherred@gmail.com>
2017-01-02 11:32:54 -0500

    remove test

--- templates/teaser.php
+++ templates/teaser.php
@@ -2,8 +2,6 @@
 $thumb_id = get_post_thumbnail_id();
 $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
 $thumb_url = $thumb_url_array[0];
-
-echo 'test';
 ?>
 
 <article class="mdl-cell mdl-cell--6-col mdl-cell--8-col-tablet teaser">
