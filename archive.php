9b5c681a9e81ae4640e7afa6af84a7b69fd5a171
Eric Sherred <esherred@gmail.com>
2017-01-05 10:49:47 -0600

    archive update

--- archive.php
+++ archive.php
@@ -14,8 +14,7 @@
           </div>
           <div class="mdl-grid">
             <?php 
-            global $query_string;
-            query_posts( $query_string . 'posts_per_page=-1' );
+            query_posts('posts_per_page=-1' );
             if (have_posts()) {
               while (have_posts()) {
                 the_post();
@@ -28,22 +27,21 @@
       </div>
       
       <?php get_template_part( 'templates/subscribe' ); ?>
-
+      
+      <?php if (have_posts()) : ?>
       <div class="mdl-grid">
         <section class="mdl-cell mdl-cell--12-col main archive">
           <div class="mdl-grid">
             <?php 
-              if (have_posts()) {
-                while (have_posts()) {
-                  the_post();
-                  get_template_part( 'templates/teaser', 'archive' );
-                }
+              while (have_posts()) {
+                the_post();
+                get_template_part( 'templates/teaser', 'archive' );
               }
-              wp_reset_postdata();
             ?>
           </div>
         </section>
       </div>
+    <?php endif; ?>
     </div>
 
     <?php get_template_part( 'templates/footer' ); ?>
