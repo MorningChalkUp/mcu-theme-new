78b0c7cbb137d91fcade07192c6696986f958d81
Eric Sherred <esherred@gmail.com>
2017-01-09 08:24:52 -0600

    oops

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
   $cat_mcu = get_category_by_slug( 'mcu' );
-  $cat_mcu_id = $cat_tidbit->term_id;
+  $cat_mcu_id = $cat_mcu->term_id;
   $cat_mcu_link = get_category_link( $cat_mcu_id );
 
   $cat_tidbits = get_category_by_slug( 'tidbits' );
