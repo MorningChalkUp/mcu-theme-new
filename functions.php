26501ff4efd9bfa9c6be2aa576b871a1995adf4f
Eric Sherred <esherred@gmail.com>
2017-01-09 11:01:35 -0600

    update cat and fields

--- functions.php
+++ functions.php
@@ -52,102 +52,6 @@ if(function_exists("register_field_group"))
     'id' => 'acf_events',
     'title' => 'Events',
     'fields' => array (
-      array (
-        'key' => 'field_57e7feddbc8e5',
-        'label' => 'Event Date',
-        'name' => 'event_date',
-        'type' => 'date_picker',
-        'date_format' => 'yymmdd',
-        'display_format' => 'dd/mm/yy',
-        'first_day' => 1,
-      ),
-      array (
-        'key' => 'field_57e80258c64b7',
-        'label' => 'Event URL',
-        'name' => 'event_url',
-        'type' => 'text',
-        'default_value' => '',
-        'placeholder' => '',
-        'prepend' => '',
-        'append' => '',
-        'formatting' => 'html',
-        'maxlength' => '',
-      ),
-      array (
-        'key' => 'field_57e82d242bf2a',
-        'label' => 'Event Address',
-        'name' => 'event_address',
-        'type' => 'text',
-        'default_value' => '',
-        'placeholder' => '',
-        'prepend' => '',
-        'append' => '',
-        'formatting' => 'html',
-        'maxlength' => '',
-      ),
-      array (
-        'key' => 'field_57e87ea3d6f20',
-        'label' => 'Event Location',
-        'name' => 'event_location',
-        'type' => 'select',
-        'choices' => array (
-          '' => '',
-          'AL' => 'AL',
-          'AK' => 'AK',
-          'AZ' => 'AZ',
-          'AR' => 'AR',
-          'CA' => 'CA',
-          'CO' => 'CO',
-          'CT' => 'CT',
-          'DE' => 'DE',
-          'FL' => 'FL',
-          'GA' => 'GA',
-          'HI' => 'HI',
-          'ID' => 'ID',
-          'IL' => 'IL',
-          'IN' => 'IN',
-          'IA' => 'IA',
-          'KS' => 'KS',
-          'KY' => 'KY',
-          'LA' => 'LA',
-          'ME' => 'ME',
-          'MD' => 'MD',
-          'MA' => 'MA',
-          'MI' => 'MI',
-          'MN' => 'MN',
-          'MS' => 'MS',
-          'MO' => 'MO',
-          'MT' => 'MT',
-          'NE' => 'NE',
-          'NV' => 'NV',
-          'NH' => 'NH',
-          'NJ' => 'NJ',
-          'NM' => 'NM',
-          'NY' => 'NY',
-          'NC' => 'NC',
-          'ND' => 'ND',
-          'OH' => 'OH',
-          'OK' => 'OK',
-          'OR' => 'OR',
-          'PA' => 'PA',
-          'RI' => 'RI',
-          'SC' => 'SC',
-          'SD' => 'SD',
-          'TN' => 'TN',
-          'TX' => 'TX',
-          'UT' => 'UT',
-          'VT' => 'VT',
-          'VA' => 'VA',
-          'WA' => 'WA',
-          'WV' => 'WV',
-          'WI' => 'WI',
-          'WY' => 'WY',
-          'INTL' => 'INTL',
-        ),
-        'default_value' => '',
-        'allow_null' => 0,
-        'multiple' => 0,
-      ),
     ),
     'location' => array (
       array (
@@ -174,36 +78,125 @@ if(function_exists("register_field_group"))
     'menu_order' => 0,
   ));
   register_field_group(array (
+    'id' => 'acf_featured-content',
+    'title' => 'Featured Content',
+    'fields' => array (
+      array (
+        'key' => 'field_58731b6c14c95',
+        'label' => 'Featured',
+        'name' => 'featured',
+        'type' => 'radio',
+        'instructions' => 'Select "Home" for homepage top content.
+  
+  Select "Section" for section area feature (smaller content).',
+        'required' => 1,
+        'choices' => array (
+          'home = Home' => 'home = Home',
+          'section = Section' => 'section = Section',
+          'none = None' => 'none = None',
+          '' => '',
+        ),
+        'other_choice' => 0,
+        'save_other_choice' => 0,
+        'default_value' => 'none',
+        'layout' => 'horizontal',
+      ),
+    ),
+    'location' => array (
+      array (
+        array (
+          'param' => 'post_category',
+          'operator' => '==',
+          'value' => '341',
+          'order_no' => 0,
+          'group_no' => 0,
+        ),
+      ),
+      array (
+        array (
+          'param' => 'post_category',
+          'operator' => '==',
+          'value' => '342',
+          'order_no' => 0,
+          'group_no' => 1,
+        ),
+      ),
+    ),
+    'options' => array (
+      'position' => 'side',
+      'layout' => 'no_box',
+      'hide_on_screen' => array (
+      ),
+    ),
+    'menu_order' => 0,
+  ));
+  register_field_group(array (
+    'id' => 'acf_quotes',
+    'title' => 'Quotes',
+    'fields' => array (
+      array (
+        'key' => 'field_5873b4c557f36',
+        'label' => 'Quote Text',
+        'name' => 'quote_text',
+        'type' => 'textarea',
+        'instructions' => 'Do not include physical quotes (") around entire quote.',
+        'required' => 1,
+        'default_value' => '',
+        'placeholder' => '',
+        'maxlength' => '',
+        'rows' => '',
+        'formatting' => 'none',
+      ),
+      array (
+        'key' => 'field_5873b50457f37',
+        'label' => 'Qute Attribution',
+        'name' => 'qute_attribution',
+        'type' => 'text',
+        'instructions' => 'Attribute the quote ',
+        'required' => 1,
+        'default_value' => '',
+        'placeholder' => '',
+        'prepend' => '',
+        'append' => '',
+        'formatting' => 'none',
+        'maxlength' => '',
+      ),
+    ),
+    'location' => array (
+      array (
+        array (
+          'param' => 'post_type',
+          'operator' => '==',
+          'value' => 'quote',
+          'order_no' => 0,
+          'group_no' => 0,
+        ),
+      ),
+    ),
+    'options' => array (
+      'position' => 'acf_after_title',
+      'layout' => 'no_box',
+      'hide_on_screen' => array (
+        0 => 'permalink',
+        1 => 'the_content',
+        2 => 'excerpt',
+        3 => 'custom_fields',
+        4 => 'discussion',
+        5 => 'comments',
+        6 => 'revisions',
+        7 => 'slug',
+        8 => 'author',
+        9 => 'format',
+        10 => 'featured_image',
+        11 => 'send-trackbacks',
+      ),
+    ),
+    'menu_order' => 0,
+  ));
+  register_field_group(array (
     'id' => 'acf_the-morning-chalk-up',
     'title' => 'The Morning Chalk Up',
     'fields' => array (
-      array (
-        'key' => 'field_586a92051b12c',
-        'label' => 'Chalk Up Tips',
-        'name' => 'mcu_tips',
-        'type' => 'wysiwyg',
-        'default_value' => '',
-        'toolbar' => 'basic',
-        'media_upload' => 'no',
-      ),
-      array (
-        'key' => 'field_586a927b1b12d',
-        'label' => 'Chalk Up Deals',
-        'name' => 'mcu_deals',
-        'type' => 'wysiwyg',
-        'default_value' => '',
-        'toolbar' => 'basic',
-        'media_upload' => 'no',
-      ),
-      array (
-        'key' => 'field_586a92981b12e',
-        'label' => 'Chalk Up Calendar',
-        'name' => 'mcu_calendar',
-        'type' => 'wysiwyg',
-        'default_value' => '',
-        'toolbar' => 'basic',
-        'media_upload' => 'no',
-      ),
     ),
     'location' => array (
       array (
@@ -225,6 +218,7 @@ if(function_exists("register_field_group"))
     'menu_order' => 0,
   ));
 }
+
 
 add_action( 'init', 'cptui_register_my_cpts' );
 function cptui_register_my_cpts() {
@@ -257,8 +251,8 @@ function cptui_register_my_cpts() {
 // End of cptui_register_my_cpts()
 }
 
-add_action( 'init', 'cptui_register_my_cpts_mcu_event' );
-function cptui_register_my_cpts_mcu_event() {
+add_action( 'init', 'cptui_register_my_cpts' );
+function cptui_register_my_cpts() {
   $labels = array(
     "name" => __( 'Events', 'morningchalkup_v2' ),
     "singular_name" => __( 'Event', 'morningchalkup_v2' ),
@@ -285,8 +279,35 @@ function cptui_register_my_cpts_mcu_event() {
     "supports" => array( "title" ),         );
   register_post_type( "mcu_event", $args );
 
-// End of cptui_register_my_cpts_mcu_event()
+  $labels = array(
+    "name" => __( 'Quotes', 'morningchalkup_v2' ),
+    "singular_name" => __( 'Quote', 'morningchalkup_v2' ),
+    );
+
+  $args = array(
+    "label" => __( 'Quotes', 'morningchalkup_v2' ),
+    "labels" => $labels,
+    "description" => "",
+    "public" => true,
+    "publicly_queryable" => true,
+    "show_ui" => true,
+    "show_in_rest" => false,
+    "rest_base" => "",
+    "has_archive" => false,
+    "show_in_menu" => true,
+        "exclude_from_search" => false,
+    "capability_type" => "post",
+    "map_meta_cap" => true,
+    "hierarchical" => false,
+    "rewrite" => array( "slug" => "quote", "with_front" => true ),
+    "query_var" => true,
+    "menu_position" => 5,"menu_icon" => "dashicons-format-quote",
+    "supports" => array( "title", "editor", "thumbnail" ),          );
+  register_post_type( "quote", $args );
+
+// End of cptui_register_my_cpts()
 }
+
 add_action( 'init', 'cptui_register_my_taxes' );
 function cptui_register_my_taxes() {
   $labels = array(
@@ -315,33 +336,6 @@ function cptui_register_my_taxes() {
 // End cptui_register_my_taxes()
 }
 
-add_action( 'init', 'cptui_register_my_taxes_event_type' );
-function cptui_register_my_taxes_event_type() {
-  $labels = array(
-    "name" => __( 'Event Types', 'morningchalkup_v2' ),
-    "singular_name" => __( 'Event Type', 'morningchalkup_v2' ),
-    );
-
-  $args = array(
-    "label" => __( 'Event Types', 'morningchalkup_v2' ),
-    "labels" => $labels,
-    "public" => true,
-    "hierarchical" => false,
-    "label" => "Event Types",
-    "show_ui" => true,
-    "show_in_menu" => true,
-    "show_in_nav_menus" => true,
-    "query_var" => true,
-    "rewrite" => array( 'slug' => 'event_type', 'with_front' => true, ),
-    "show_admin_column" => false,
-    "show_in_rest" => false,
-    "rest_base" => "",
-    "show_in_quick_edit" => false,
-  );
-  register_taxonomy( "event_type", array( "mcu_event" ), $args );
-
-// End cptui_register_my_taxes_event_type()
-}
 
 $state['AK'] = 'Alaska';
 $state['AZ'] = 'Arizona';
