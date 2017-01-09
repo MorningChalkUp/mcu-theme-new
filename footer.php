a0968e68e98cc0ca28e400b02f3507735469e836
Eric Sherred <esherred@gmail.com>
2017-01-09 10:17:47 -0600

    textfit

--- footer.php
+++ footer.php
@@ -3,6 +3,7 @@
     <script src="<?php echo get_template_directory_uri(); ?>/js/jquery-2.2.3.min.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/js/material.min.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/js/getmdl-select.min.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.textfit.min.j"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/js/init.js"></script>
 
     <?php wp_footer(); ?>
