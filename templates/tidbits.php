9782b6424574fbf8373638780d8540b6024eb913
Eric Sherred <esherred@gmail.com>
2017-01-09 10:03:54 -0600

    rm quote temp content

--- templates/tidbits.php
+++ templates/tidbits.php
@@ -66,13 +66,13 @@
     ?>
     
     <?php
-      $quote = new WP_Query($quote_args);
+     /* $quote = new WP_Query($quote_args);
       if ($quote->have_posts())
         while($quote->have_posts()) {
           $quote->the_post();
           get_template_part( 'templates/quote' );
         }
-      }
+      }*/
     ?>
 
     <?php
