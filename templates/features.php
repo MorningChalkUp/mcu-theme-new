20382294d9cd4eee269189e2763a6475dd647ec0
Eric Sherred <esherred@gmail.com>
2017-01-09 08:44:33 -0600

    VAR

--- templates/features.php
+++ templates/features.php
@@ -18,8 +18,11 @@
       </article>
       
       <?php 
-        $mcu = new WP_Query(array( 'posts_per_page' => 5 ));
-        var_dump($mcu);
+        $mcu_args = array (
+          'cat'               => $cat_mcu_id,
+          'posts_per_page'    => 5,
+        );
+        $mcu = new WP_Query($mcu_args);
         if ( $mcu->have_posts() ) {
           while( $mcu->have_posts() ) {
             $mcu->the_post();
