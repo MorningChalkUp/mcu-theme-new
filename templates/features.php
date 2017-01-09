2254049f361935878a97d2922ef5fd172b08deb1
Eric Sherred <esherred@gmail.com>
2017-01-09 08:02:29 -0600

    try moving loops

--- templates/features.php
+++ templates/features.php
@@ -1,47 +1,41 @@
 <?php
-  // Home Feature
-  $feature_args = array (
-    'cat' => array($cat_story_id,$cat_tidbits_id),
-    'posts_per_page'  => 1,
-    'meta_query'      => array(
-      array(
-        'key' => 'featured',
-        'compare' => '=',
-        'value' => 'home',
-      ),
-    ),
-  );
-
-  $feature = new WP_Query($feature_args);
-
-  if ($feature->post_count != 1) {
-    $feature_args = array (
-      'cat' => array($cat_story_id,$cat_tidbits_id),
-      'posts_per_page'  => 1,
-    );
-    $feature = new WP_Query($feature_args);
-  }
-
-  $exclude[] = wp_list_pluck( $tidbit_feature->posts, 'ID' );
-
-  // MCU
-  $mcu_args = array (
-    'cat' => $cat_mcu_id,
-    'posts_per_page'  => 5,
-  );
-
-  $mcu = new WP_Query($args);
+  
 ?>
 
 <section class="feature mdl-grid">
   
   <?php 
+    $feature_args = array (
+      'cat' => array($cat_story_id,$cat_tidbits_id),
+      'posts_per_page'  => 1,
+      'meta_query'      => array(
+        array(
+          'key' => 'featured',
+          'compare' => '=',
+          'value' => 'home',
+        ),
+      ),
+    );
+
+    $feature = new WP_Query($feature_args);
+
+    if ($feature->post_count != 1) {
+      $feature_args = array (
+        'cat' => array($cat_story_id,$cat_tidbits_id),
+        'posts_per_page'  => 1,
+      );
+      $feature = new WP_Query($feature_args);
+    }
+
+    $exclude[] = wp_list_pluck( $tidbit_feature->posts, 'ID' );
+
     if ($feature->have_posts()) {
       white($feature->have_posts()) {
         $feature->the_post();
         get_template_part( 'templates/feature' );
       }
     }
+    wp_reset_postdata();
   ?>
 
   <div class="mcu-roll mdl-cell mdl-cell--4-col mdl-cell--order-1-desktop">
@@ -54,12 +48,20 @@
       </article>
       
       <?php 
+        $mcu_args = array (
+          'cat' => $cat_mcu_id,
+          'posts_per_page'  => 5,
+        );
+
+        $mcu = new WP_Query($args);
+
         if ($mcu->have_posts()) {
           white($mcu->have_posts()) {
             $mcu->the_post();
             get_template_part( 'templates/mcu' );
           }
         }
+        wp_reset_postdata();
       ?>
 
     </div>
