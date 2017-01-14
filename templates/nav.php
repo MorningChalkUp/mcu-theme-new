33aff740a6b96f1e527bf23b65ead3dee9250b07
Eric Sherred <esherred@gmail.com>
2017-01-14 01:39:58 -0600

    nav update

--- templates/nav.php
+++ templates/nav.php
@@ -17,14 +17,14 @@
       </a>
     </div>
     <div class="right search">
-      <a href="/search">
+      <a href="/search/">
         <i class="mdi mdi-magnify"></i>
       </a>
     </div>
   </div>
 </header>
 <div class="mdl-layout__drawer mdl-layout--small-screen-only">
-  <span class="mdl-layout-title"><a href="#"><span>Morning</span> Chalk Up</a></span>
+  <span class="mdl-layout-title"><a href="/"><span>Morning</span> Chalk Up</a></span>
   <nav class="mdl-navigation">
     <?php 
       $array_menu = wp_get_nav_menu_items('Main Nav');
