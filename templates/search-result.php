1b757c08c7e03ead0d294a7dd396e52435d8d7ed
Eric Sherred <esherred@gmail.com>
2017-01-14 00:45:42 -0600

    meta

--- templates/search-result.php
+++ templates/search-result.php
@@ -1,4 +1,14 @@
 <article>
   <div class="title"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></div>
+  <div class="meta">
+    <?php 
+      $categories = get_the_category( $post->ID );
+      if ( ! empty( $categories ) ) {
+          echo '<a href="' . esc_url( get_category_link( $categories[0]->term_id ) ) . '">' . esc_html( $categories[0]->name ) . '</a>';
+      }
+    ?>
+    •
+    <?php echo get_the_date('M j, Y'); ?>
+  </div>
   <div class="excerpt"><?php the_excerpt(); ?></div>
 </article>
