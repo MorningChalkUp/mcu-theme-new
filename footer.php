3fe069b43971a1b1df2e299587ee156b3b3e46f6
Eric Sherred <esherred@gmail.com>
2017-01-21 08:59:06 -0600

    overlay

--- footer.php
+++ footer.php
@@ -1,4 +1,6 @@
-</div>
+    </div>
+      
+    <div class="overlay"></div>
 
     <script src="<?php echo get_template_directory_uri(); ?>/js/jquery-2.2.3.min.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/js/material.min.js"></script>
