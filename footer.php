d2efe612c0008bcb75e8fe78586ba42f09159722
Eric Sherred <esherred@gmail.com>
2017-01-27 14:46:15 -0600

    move scripts for lazy load

--- footer.php
+++ footer.php
@@ -2,12 +2,6 @@
       
     <div class="overlay"></div>
 
-    <script src="<?php echo get_template_directory_uri(); ?>/js/material.min.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/js/getmdl-select.min.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.textfit.min.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/immybox/jquery.immybox.min.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/js/init.js"></script>
-
     <script>
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
