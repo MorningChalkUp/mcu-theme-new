164466e06d64263e3026af967c6fb87c7d47506c
Eric Sherred <esherred@gmail.com>
2017-02-07 15:14:57 -0600

    update tags to go through Google Tag Manager

--- footer.php
+++ footer.php
@@ -10,31 +10,6 @@
     <script src="<?php echo get_template_directory_uri(); ?>/js/mdl-selectfield.min.js"></script>
     <script defer src="<?php echo get_template_directory_uri(); ?>/js/init.js"></script>
 
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-76434343-1', 'auto');
-        ga('send', 'pageview');
-    </script>
-    <!-- Google Code for Remarketing Tag -->
-    <script type="text/javascript">
-    /* <![CDATA[ */
-    var google_conversion_id = 861338482;
-    var google_custom_params = window.google_tag_params;
-    var google_remarketing_only = true;
-    /* ]]> */
-    </script>
-    <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
-    </script>
-    <noscript>
-    <div style="display:inline;">
-    <img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/861338482/?guid=ON&amp;script=0"/>
-    </div>
-    </noscript>
-
     <?php wp_footer(); ?>
 
   </body>
--- header.php
+++ header.php
@@ -28,36 +28,15 @@
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 
-    <script>
-      !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-      n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
-      n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
-      t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
-      document,'script','https://connect.facebook.net/en_US/fbevents.js');
-
-      fbq('init', '253166851706578');
-      fbq('track', "PageView");
-      <?php if (is_page( 'thank-you' )): ?>
-      fbq('track', 'CompleteRegistration');
-      <?php endif; ?>
-      </script>
-      <noscript><img height="1" width="1" style="display:none"
-      src="https://www.facebook.com/tr?id=253166851706578&ev=PageView&noscript=1"
-      /></noscript>
-      <!-- Start Visual Website Optimizer Asynchronous Code -->
-      <script type='text/javascript'>
-      var _vwo_code=(function(){
-      var account_id=262907,
-      settings_tolerance=2000,
-      library_tolerance=2500,
-      use_existing_jquery=false,
-      // DO NOT EDIT BELOW THIS LINE
-      f=false,d=document;return{use_existing_jquery:function(){return use_existing_jquery;},library_tolerance:function(){return library_tolerance;},finish:function(){if(!f){f=true;var a=d.getElementById('_vis_opt_path_hides');if(a)a.parentNode.removeChild(a);}},finished:function(){return f;},load:function(a){var b=d.createElement('script');b.src=a;b.type='text/javascript';b.innerText;b.onerror=function(){_vwo_code.finish();};d.getElementsByTagName('head')[0].appendChild(b);},init:function(){settings_timer=setTimeout('_vwo_code.finish()',settings_tolerance);var a=d.createElement('style'),b='body{opacity:0 !important;filter:alpha(opacity=0) !important;background:none !important;}',h=d.getElementsByTagName('head')[0];a.setAttribute('id','_vis_opt_path_hides');a.setAttribute('type','text/css');if(a.styleSheet)a.styleSheet.cssText=b;else a.appendChild(d.createTextNode(b));h.appendChild(a);this.load('//dev.visualwebsiteoptimizer.com/j.php?a='+account_id+'&u='+encodeURIComponent(d.URL)+'&r='+Math.random());return settings_timer;}};}());_vwo_settings_timer=_vwo_code.init();
-      </script>
-      <!-- End Visual Website Optimizer Asynchronous Code -->
-
-
     <?php wp_head(); ?>
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5MZWLQK');</script>
+    <!-- End Google Tag Manager -->
   </head>
   <body>
     <?php get_template_part( 'templates/fb' ); ?>
