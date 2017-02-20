93c5fd69ac123f0e6106e4d65f15866b5e744716
Eric Sherred <esherred@gmail.com>
2017-02-20 11:34:39 -0600

    update main sub to remove website

--- page-subscribe.php
+++ page-subscribe.php
@@ -125,27 +125,24 @@
                   <div class="mdl-layout-spacer"></div>
 
                   <div class="mdl-cell mdl-cell--4-col">
-                    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                      <input class="mdl-textfield__input" type="text" id="website" name="website">
-                      <label class="mdl-textfield__label" for="website">Website</label>
-                    </div>
-                  </div>
-
-                  <div class="mdl-layout-spacer"></div>
-
-                  <div class="mdl-cell mdl-cell--4-col">
                     Subscribe to:
                     
                     <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="us">
                       <input type="checkbox" id="us" class="mdl-checkbox__input" name="us">
                       <span class="mdl-checkbox__label">Morning Chalk Up</span>
                     </label>
-
+                    
                     <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="eu">
                       <input type="checkbox" id="eu" class="mdl-checkbox__input" name="eu">
                       <span class="mdl-checkbox__label">Morning Chalk Up Europe</span>
                     </label>
 
+                  </div>
+                  
+                  <div class="mdl-layout-spacer"></div>
+
+                  <div class="mdl-cell mdl-cell--4-col">
+                    &nbsp;
                   </div>
                   
                   <div class="mdl-layout-spacer"></div>
