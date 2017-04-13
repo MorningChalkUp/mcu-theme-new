52477b3dabda357ca834f353a5dd4b31ef098694
Eric Sherred <esherred@gmail.com>
2017-04-13 00:07:43 -0500

    wide template w bg (static bg)
    TODO: add bg based on featured image

---
+++ wide-page.php
@@ -0,0 +1,49 @@
+<?php
+// Template Name: Full Width w Background
+?>
+
+<?php get_header(); ?>
+
+<main class="mdl-layout__content">
+  <div class="page-content">
+
+    <article class="main subscribe-page">
+      <section class="hero">
+        <div class="mdl-grid">
+          <div class="mdl-cell mdl-cell--12-col mdl-grid">
+            <div class="mdl-layout-spacer"></div>
+            <div class="mdl-cell mdl-cell--10-col form">
+              <div class="mdl-grid">
+                
+                <div class="mdl-layout-spacer"></div>
+
+                <div class="mdl-cell mdl-cell--10-col headline">
+                  <?php the_title(); ?>
+                </div>
+
+                <div class="mdl-layout-spacer"></div>
+              </div>
+
+              <div class="mdl-grid">
+                
+                <div class="mdl-layout-spacer"></div>
+
+                <div class="mdl-cell mdl-cell--10-col">
+                  <?php the_content(); ?>
+                </div>
+
+                <div class="mdl-layout-spacer"></div>
+              </div>
+              
+            </div>
+            <div class="mdl-layout-spacer"></div>
+          </div>
+        </div>
+      </section>
+    </article>
+
+  <?php get_template_part( 'templates/footer' ); ?>
+
+</main>
+
+<?php get_footer(); ?>
