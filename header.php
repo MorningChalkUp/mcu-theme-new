b56e3686ffcf6f3203f710cda93cb0e2cf1dda67
Eric Sherred <esherred@gmail.com>
2017-07-25 11:08:22 -0500

    ad fb:pages meta to header

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="fb:pages" content="198171777207430" />
     
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
