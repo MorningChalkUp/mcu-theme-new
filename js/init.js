af26c8259207a53a836be3b7ed788f5288124b8f
Eric Sherred <esherred@gmail.com>
2017-09-01 23:35:15 -0500

    fb btn pages must hold the scripts, not init

--- js/init.js
+++ js/init.js
@@ -4,36 +4,6 @@ $(document).ready(function(){
 
 });
 
-function statusChangeCallback(response) {
-  if (response.status === 'connected') {
-    mcu_fb_signup();
-  }
-}
-
-function checkLoginState() {
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-}
-
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9&appId=1635216993444923";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-function mcu_fb_signup() {
-  FB.api('/me?fields=name,email', function(response) {
-    $('#FBID').val(response.id);
-    $('#full-name').val(response.name);
-    $('#email').val(response.email);
-    $('#about').val('CrossFit Fan!');
-    $('#subscribe').submit();
-  });
-}
-
 function toggel(id) {
   if ($(id).hasClass('hidden')) {
     $(id).removeClass('hidden');
