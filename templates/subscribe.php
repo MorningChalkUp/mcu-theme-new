3f3f1296da9e5746d95ab7112ae518ce082f2dc6
Matthew Masoni <matthewmasoni@Mats-MBP.local>
2018-04-03 18:28:50 -0700

    added special feature to options panel

--- templates/subscribe.php
+++ templates/subscribe.php
@@ -1,11 +1,16 @@
+<?php 
+  global $wp;
+  $bgs = get_field('banner_images','options');
+  shuffle($bgs);
+  $feature = get_field('feature_status','options');
+?>
+
 <section class="mdl-grid ">
-  <div class="mdl-layout-spacer"></div>
+  <?php if(!$feature) echo "<div class=\"mdl-layout-spacer\"></div>"; ?>
+  
   <div class="mdl-cell mdl-cell--8-col">
-    <?php 
-      $bgs = get_field('banner_images','options');
-      shuffle($bgs);
-    ?>
-    <div class="subscribe" style="background-image:url('<?php echo $bgs[0]['url'] ?>');" >
+
+    <div class="cta-box subscribe" style="background-image:url('<?php echo $bgs[0]['url'] ?>');" >
       <h3>Get The Morning Chalk Up</h3>
       <p>Delivered to your inbox every morning.</p>
       <form class="pill-form" action="/process/sub-box.php" method="post">
@@ -21,5 +26,16 @@
       </form>
     </div>
   </div>
-  <div class="mdl-layout-spacer"></div>
+  
+  <?php if($feature): $link = get_field('feature_link','options'); $bg = get_field('feature_background','options'); ?>
+  <div class="mdl-cell mdl-cell--4-col mdl-cell--12-col-tablet">
+    <div class="cta-box merch" style="background-image:url('<?php echo $bg['sizes']['large'] ?>');">
+      <h3><?php the_field('feature_title','options') ?></h3>
+      <p><?php the_field('feature_text','options') ?></p>
+      <p><a class="btn" href="<?php echo $link['url'] ?>?utm_source=morningchalkup&utm_medium=banner&utm_campaign=onsite&utm_content=<?php echo home_url( $wp->request ) ?>" target="<?php echo $link['target'] ?>"><?php echo $link['title'] ?></a></p>
+    </div>
+  </div>
+  <?php endif; ?>
+  
+  <?php if(!$feature) echo "<div class=\"mdl-layout-spacer\"></div>"; ?>
 </section>
